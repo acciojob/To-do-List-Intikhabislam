42000198bbb4f7e1d41f427c18de9fbed9273455
Intikhabislam <124589066+Intikhabislam@users.noreply.github.com>
2023-03-18 11:17:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
+const newTodoInput=document.getElementById("newTodoInput");
+const addTodoBtn=document.getElementById("addTodoBtn");
+const todoList =document.getElementById("todoList");
+
+addTodoBtn.addEventListener("click", function()){
+	const newTodo=newTodoInput.value.trim();
+	if(newTodo !=="")
+	{
+		const newTodoItem=document.createElement("li");
+		newTodoItem.textContent=newTodo;
+		todolist.appendChild(newTodoItem);
+		newTodoInput.value="";
+	}
+});
+
 
